33d6cd40571fd9b0f7eefc2ac7a3f952719f428d
Jackie <jacgut19@gmail.com>
2017-12-03 22:54:52 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//Add Two Numbers with JavaScript
+
+var sum = 10 + 10;
+
+sum = 20
+
+
+//Subtract One Number from Another with JavaScript 
+
+var difference = 45 - 33;
+
+difference = 12
+
+
+//Multiply Two Numbers with JavaScript
+
+var product = 8 * 10;
+
+product = 80
+
+
+//Divide One Number by Another with JavaScript
+
+var quotient = 66 / 33;
+
+quotient = 2
+
+
+
