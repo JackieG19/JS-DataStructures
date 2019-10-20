fe7312d1d15efe289b5f8182cf3028bb5168fc93
Jackie <JGtheophila19@gmail.com>
2019-10-20 19:15:41 -0400

    Combining arrays with the spread operator

---
+++ spreadOp.js
@@ -0,0 +1,14 @@
+function spreadOut() {
+
+  let fragment = ['to', 'code'];
+  
+  let sentence; // change this line
+  
+  return sentence = ['learning', ...fragment, 'is', 'fun'];
+}
+
+console.log(spreadOut());
+
+/*
+The spread operator hass the ability to combine arrays, or insert all the elements of one array into another at any index.
+Can concatenate arrays, but this only allows to combine arrays at the end of one, and at the start of another*/
